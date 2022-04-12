d77f59cb94adc24c3e865caf919d8218f29f747e
Nathan Farley <nathanfarley@Nathans-MacBook-Pro.local>
2022-04-12 10:34:10 -0500

    Changes url string in db.

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const connectionString = '"mongodb+srv://nathan:Hamptonej1!@nathan.igthh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"'
+const connectionString = 'mongodb+srv://nathan:Hamptonej1!@nathan.igthh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 let db
 
