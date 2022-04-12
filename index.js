9d2340c8e3fd23af49bac0e723610ebbe1f3a726
Nathan Farley <nathanfarley@Nathans-MacBook-Pro.local>
2022-04-12 10:09:37 -0500

    Removes usage of express sserve.

--- index.js
+++ index.js
@@ -132,10 +132,3 @@ app.get('/bank/users', async (req, res) => {
 
     res.json({ users })
 })
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../', 'build')));
-    app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../', 'build/index.html'), err => res.status(500).send(process.cwd() + '   --' + 'UHSDA: ' + err));
-    })
-  }
